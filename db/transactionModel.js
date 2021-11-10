a8c729c200b2cf5375b3a1876ae1e48dc38ff293
Mykola Kizan <mkizan@gmail.com>
2021-11-10 14:02:36 +0200

    add transaction schema

--- db/transactionModel.js
+++ db/transactionModel.js
@@ -1,28 +1,61 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 // const mongoosePaginate = require('mongoose-paginate-v2')
 
-const transactionSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const transactionSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: [true, "Выберите тип транзакции"],
+      enum: ["Доход", "Расход"],
+    },
+    category: {
+      type: String,
+      enum: [
+        "Основной",
+        "Еда",
+        "Авто",
+        "Развитие",
+        "Дети",
+        "Дом",
+        "Образование",
+        "Остальные",
+      ],
+      required: [true, "Выберите категорию"],
+    },
+    comment: {
+      type: String,
+    },
+    amount: {
+      type: Number,
+      required: [true, "Укажите сумму транзакции"],
+    },
+    // balance: {
+    //   type: Number,
+    //   required: true,
+    // },
   },
+  { timestamps: true }
+);
 
-  email: {
-    type: String,
-  },
-
-  phone: {
-    type: String,
-  },
-
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-})
+const joiSchema = Joi.object({
+  date: Joi.date().required(),
+  type: Joi.string().required(),
+  category: Joi.string().required(),
+  comment: Joi.string(),
+  amount: Joi.number().required(),
+  // balance: Joi.number().required(),
+});
 
 // contactSchema.plugin(mongoosePaginate)
 
-const Transaction = model('Transaction', transactionSchema)
+const Transaction = model("Transaction", transactionSchema);
 
-module.exports = { Transaction }
+module.exports = { Transaction, joiSchema };
