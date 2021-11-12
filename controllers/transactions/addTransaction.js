621f13a36544eb60fe6842f9431f4ec9fc38b063
Mykola Kizan <mkizan@gmail.com>
2021-11-12 17:21:26 +0200

    added rule in 'getAllTransactions'

--- controllers/transactions/addTransaction.js
+++ controllers/transactions/addTransaction.js
@@ -1,5 +1,4 @@
 const Joi = require("joi");
-const { getUserByToken } = require("../../model/users");
 const { createTransaction } = require("../../model/transactions");
 
 const joiSchema = Joi.object({
--- controllers/transactions/getAllTransactions.js
+++ controllers/transactions/getAllTransactions.js
@@ -1,15 +1,23 @@
 const { getTransactions } = require("../../model/transactions");
-// const { Transaction } = require("../../db/transactionModel");
-// const { getUserByToken } = require("../../model/users");
 
 const getAllTransactions = async (req, res) => {
   const { _id } = req.user;
   const allTransactions = await getTransactions(_id);
   console.log(allTransactions);
-  // якщо пустий массив
-  // if (allTransactions.length === 0) {
-  //   res.status(204).json();
-  // }
+
+  // якщо массив порожній
+  if (allTransactions.length === 0) {
+    res.status(200).json({
+      Status: "200 OK",
+      "Content-Type": "application/json",
+      ResponseBody: {
+        message: "You have no transactions yet",
+      },
+    });
+
+    return;
+  }
+
   res.status(200).json({
     Status: "200 OK",
     "Content-Type": "application/json",
