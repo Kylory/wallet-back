e7227b3e7a2e09a60c37b3a9f8e37fbe592f82ca
Kyrylo Moskalenko <krllmsk@gmail.com>
2021-11-12 18:32:37 +0200

    installed swagger-ui-express, added swagger.json

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const transactionsRouter = require("./routes/api/transactions");
-const usersRouter = require("./routes/api/users");
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
-const app = express();
+const transactionsRouter = require('./routes/api/transactions')
+const usersRouter = require('./routes/api/users')
 
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+const app = express()
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 // app.use(express.static('public'))
 
-app.use("/api/transactions", transactionsRouter);
-app.use("/api/users", usersRouter);
+app.use('/api/transactions', transactionsRouter)
+app.use('/api/users', usersRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+  res.status(404).json({ message: 'Not found' })
+})
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+  const { status = 500, message = 'Server error' } = err
+  res.status(status).json({ message })
+})
 
-module.exports = app;
+module.exports = app
